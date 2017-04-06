d5c2a126c87de8c354fc8faf66d153ab2cc14a72
RaghavaDhanya <raghava.dhanya@gmail.com>
2017-04-07 00:48:46 +0530

    Image loading and menuloop !

--- main.cpp
+++ main.cpp
@@ -4,14 +4,16 @@
 #else
 #include <GL/glut.h>
 #endif
-
+#include <GL/freeglut.h>
 #include <stdlib.h>
-
+#include "lib/lodepng.h"
+#include "lib/lodepng.cpp"
+#include <iostream>
 int HEIGHT = 480;
 int WIDTH = 640;
 bool PRESSED=false;
-
-namespace states
+using namespace std;
+namespace Rstates
 {
     const int MENU=0;
     const int GAME=1;
@@ -19,15 +21,69 @@ namespace states
 
     int STATE=MENU;
 }
+namespace Rimages
+{
+    const char* logoName="morse.png";
+    vector<unsigned char> logo;
+    unsigned logoWidth=326;
+    unsigned logoHeight=326;
+    void invert(vector<unsigned char> &img,const unsigned width,const unsigned height)
+    {
+        unsigned char *imagePtr = &img[0];
+        unsigned char *top = NULL;
+        unsigned char *bottom = NULL;
+        unsigned char temp = 0;
+        for( int h = 0; h <(int) height/2; ++h )
 
+        {
+
+            top = imagePtr + h * width * 4;
+            bottom = imagePtr + (height - h - 1) * width*4;
+            for( int w = 0; w < (int)width*4; ++w )
+            {
+                temp = *top;
+                *top = *bottom;
+                *bottom = temp;
+                ++top;
+                ++bottom;
+            }
+        }
+        }
+    void loadImages()
+    {
+        int error;
+        if((error=lodepng::decode(logo,logoWidth,logoHeight,logoName)))
+        {
+            cout<<lodepng_error_text(error)<<endl;
+            exit(1);
+        }
+        else
+            invert(logo,logoWidth,logoHeight);
+
+    }
+
+}
+//Set a letter on top of screen, used during game loop
 void setLetter(char ch)
 {
-    glLineWidth(2);
+    glLineWidth(5);
     glColor3ub(0x42,0x42,0x42);
     glPushMatrix();
     glTranslatef(WIDTH/2.0-50,HEIGHT-110,0);
     glutStrokeCharacter(GLUT_STROKE_ROMAN, (int)ch);
+    //glutStrokeString(GLUT_STROKE_MONO_ROMAN,(unsigned char*)"abcdef");
     glPopMatrix();
+}
+void menuLoop()
+{
+    glLineWidth(5);
+    //glColor3ub(0x42,0x42,0x42);
+    glPushMatrix();
+    glTranslatef(WIDTH/2.0-250,HEIGHT-110,0);
+    glutStrokeString(GLUT_STROKE_ROMAN,(unsigned char*)"ReMorse");
+    glPopMatrix();
+    glRasterPos2i(WIDTH/2-(Rimages::logoWidth/2),0);
+    glDrawPixels(Rimages::logoWidth,Rimages::logoHeight, GL_RGBA, GL_UNSIGNED_BYTE, &Rimages::logo[0]);
 }
 static void resize(int width, int height)
 {
@@ -48,7 +104,12 @@ static void display(void)
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     //Draw stuff here
-    setLetter('R');
+    //setLetter('R');
+    switch(Rstates::STATE)
+    {
+    case Rstates::MENU:
+        menuLoop();break;
+    }
     glutSwapBuffers();
 }
 
@@ -103,8 +164,9 @@ static void idle(void)
 
 int main(int argc, char *argv[])
 {
+    Rimages::loadImages();
     glutInit(&argc, argv);
-    glutInitWindowSize(640,480);
+    glutInitWindowSize(WIDTH,HEIGHT);
     glutInitWindowPosition(10,10);
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
 
@@ -117,14 +179,13 @@ int main(int argc, char *argv[])
     glutSpecialFunc(splkey);
     glutSpecialUpFunc(splkeyup);
     glutIdleFunc(idle);
-
     //Do anti alias
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_BLEND);
     glEnable(GL_POINT_SMOOTH);
     glEnable(GL_LINE_SMOOTH);
     glEnable(GL_POLYGON_SMOOTH);
-//  glEnable(GL_MULTISAMPLE);
+//    glEnable(GL_MULTISAMPLE);
     //make key not repeat events on long press
     glutSetKeyRepeat(0);
 
