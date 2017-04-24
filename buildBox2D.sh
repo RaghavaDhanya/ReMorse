f38c73ae9cc170c6746513d0998f063537ebc482
RaghavaDhanya <raghava.dhanya@gmail.com>
2017-04-24 19:27:47 +0530

    Corrected compile.sh!

--- buildBox2D.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/erincatto/Box2D
-cd Box2D/Box2D
-git branch pk v2.3.0
-git checkout pk
-cmake .
-make
--- compile.sh
+++ compile.sh
@@ -4,6 +4,6 @@ g++ -c main.cpp -o extra/main.o
 g++ -c states.cpp -o extra/states.o
 g++ -c keys.cpp -o extra/keys.o
 g++ -c timer.cpp -o extra/timer.o
-g++ -c physics.cpp -lBox2D -lGL -o extra/physics.o
+g++ -c  -std=c++11 physics.cpp -lBox2D -lGL -o extra/physics.o
 g++ extra/main.o extra/states.o extra/keys.o extra/timer.o extra/physics.o -o remorse.out -lGL -lGLU -lglut
 rm -rf extra
