0813b56aba07d6129d6d9b83a270d225c300a043
Ravikiran <kienme@gmail.com>
2017-04-29 15:03:47 +0530

    Update compile.sh Moved compiling of main to after physics, because main requires physics to be compiled first

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 mkdir extra
-g++ -c main.cpp -o extra/main.o
 g++ -c states.cpp -o extra/states.o
 g++ -c keys.cpp -o extra/keys.o
 g++ -c timer.cpp -o extra/timer.o
 g++ -c  -std=c++11 physics.cpp -lBox2D -lGL -o extra/physics.o
+g++ -c main.cpp -o extra/main.o
 g++ extra/main.o extra/states.o extra/keys.o extra/timer.o extra/physics.o -o remorse.out -lGL -lGLU -lglut
 rm -rf extra
